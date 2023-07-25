66f7a96111598756d7ff071894b5bc6427837452
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2023-07-25 05:59:38 -0400

    added explanation comments in the Elixir comments

--- src/Task1/Task1.ex
+++ src/Task1/Task1.ex
@@ -1,17 +1,18 @@
-defmodule Divisibility do
-  def check_numbers do
+defmodule Divisibility do # Module declaration for the better organisation code
+  def check_numbers do # Public function declaration
+    # Generator, generates a number from 1 to 100 and passes that number to the check_divisibility function
     for n <- 1..100, do: check_divisibility(n)
   end
 
-  defp check_divisibility(n) do
-    cond do
-      rem(n, 3) == 0 and rem(n, 5) == 0 -> "Div3&5"
-      rem(n, 3) == 0 -> "Div3"
-      rem(n, 5) == 0 -> "Div5"
-      true -> n
+  defp check_divisibility(n) do # private function declaration
+    cond do # if else statements
+      rem(n, 3) == 0 and rem(n, 5) == 0 -> "Div3&5" # return Div3&5 if the number is divisible by 3 and 5
+      rem(n, 3) == 0 -> "Div3" # return Div3 if the number is divisible by 3
+      rem(n, 5) == 0 -> "Div5" # return Div5 if the number is divisible by 5
+      true -> n # return the number itself
     end
   end
 end
 
-# Testing the program
+# Print results
 IO.inspect(Divisibility.check_numbers())
